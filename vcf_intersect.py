3cbc68f2ef236c58d9793477da713792f0d58d33
indapa <indapa@gmail.com>
2011-02-05 13:35:15 -0500

    bitset build of VCF ignores filtered records

--- vcf_intersect.py
+++ vcf_intersect.py
@@ -52,7 +52,7 @@ def binned_bitsets_from_vcffile( f, chrom_col=0, start_col=1,  upstream_pad=0, d
             else:
                 continue
 
-
+        print filter
         chrom="chr"+chrom
         if chrom != last_chrom:
             if chrom not in bitsets:

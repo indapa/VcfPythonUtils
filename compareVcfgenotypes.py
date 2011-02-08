1b98e8828afad2de88efb76fb44bdac8320c1aac
indapa <indapa@gmail.com>
2011-02-08 00:28:58 -0500

    report nocalls in eval and comparison set in output

--- compareVcfgenotypes.py
+++ compareVcfgenotypes.py
@@ -119,7 +119,7 @@ def main():
 
         for (g1, g2, sample) in comparison_results:
             discordance_dict[sample][g1,g2]+=1
-    print "sample","NRD", "NRS", "totalGenotypes", "noCallsEval", "noCallsComparison"
+    print "sample","NRD", "NRS", "totalGenotypes", "noCallsEval"
     for sample in discordance_dict.keys():
         #print sample
         #print discordance_dict[sample]
@@ -127,10 +127,9 @@ def main():
 
         nrc=computeNRS( discordance_dict[sample] )
         nrd=computeNRD(  discordance_dict[sample]  )
-        missing_genotypes = discordance_dict[sample][3,0]
-        missing_genotypes_comparison= discordance_dict[sample][0,3]
-
-        print sample,nrd, nrc,  np.sum( discordance_dict[sample] ) , np.sum( missing_genotypes ), np.sum( missing_genotypes_comparison )
-        #print "=="
+        row_marginal_sums = discordance_dict[sample].sum(axis=1)
+        colum_marginal_sums = discordance_dict[sample].sum(axis=1)
+        #print discordance_dict[sample]
+        print sample,nrd, nrc,  np.sum( discordance_dict[sample] ) , np.sum(row_marginal_sums[3]), np.sum(colum_marginal_sums[3])
 if __name__ == "__main__":
     main()

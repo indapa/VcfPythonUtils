3e242826dc566a15f374498127f40e9dc8a96e03
indapa <indapa@gmail.com>
2011-07-03 15:57:40 -0400

    added isHet and checkFormatIds  functions

--- VcfGenotype.py
+++ VcfGenotype.py
@@ -11,12 +11,12 @@ class VcfGenotype(object):
         self.isPhased=0
         self.allele1=''
         self.allele2=''
-        #keys are format key value is from gstring
 
+        #keys are format key value is from gstring
         self.gdict={}
         formatids=self.formatstring.split(':')
         gstringvals=self.gstring.split(':')
-
+        self.parseAlleles(gstringvals[0])
 
         if gstring != '.':
             if len(formatids) != len(gstringvals):
@@ -53,6 +53,10 @@ class VcfGenotype(object):
             delimiter='|'
         elif '/' in allelefield:
             delimiter='/'
+        elif '.' == allelefield:
+            self.allele1='.'
+            self.allele2='.'
+            return
         else:
             sys.stderr.write("un-recognized genotype delimiter: " + allelefield + "\n")
             exit(1)
@@ -63,6 +67,27 @@ class VcfGenotype(object):
 
     def getFormatString(self):
         return self.formatstring
+
+    def getAlleles(self):
+        """ return tuple with (allele1, allele2) """
+        return (self.allele1, self.allele2)
+
+    def isHet(self):
+        """ return True if gentoype is het and both alleles are called"""
+        
+        if self.allele1 != '.' and self.allele2 != '.':
+            if self.allele1 != self.allele2:
+                return True
+        return False
+    def checkFormatIds(self, formatlist):
+        """ check the ids in the FORMAT field to make sure they are contained in the list of FORMAT ids (the format list) """
+        formatfields=self.formatstring.split(':')
+        for elem in formatfields:
+            if elem not in self.gdict.keys():
+                sys.stderr.write(elem + " is not in FORMAT column!\n ")
+                exit(1)
+
+
 
     def toString(self):
         return self.gstring

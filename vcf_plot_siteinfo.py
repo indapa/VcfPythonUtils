6c5eff880570cbc65958a554e549e48caae0785f
indapa <indapa@gmail.com>
2011-07-20 12:33:42 -0400

    initial commit

---
+++ vcf_plot_siteinfo.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+import sys
+import os
+import string
+import re
+from optparse import OptionParser
+from numpy import *
+from VcfFile import *
+import rpy2.robjects as robjects
+from rpy2.robjects.packages import importr
+
+
+""" print 5 number summary (min, max, first & third quartile, mean, and median of   INFO (as well as QUAL)  tags  attribute at sites in a VCF file and generate an Rplot of the percentiles """
+
+def main():
+    usage = "usage: %prog [options] file.vcf"
+    parser = OptionParser(usage)
+    usage = "usage: %prog [options] file.vcf \n print 5 number summary  information about --info tag ids  in records of a VCF file\n"
+    parser = OptionParser(usage)
+    parser.add_option("--info", type="string", dest="infotag", help="INFO tag attribute to get stats for", default="")
+    parser.add_option("--max", type="int", dest="max", help="skip records that are greater than or equal to max (default sys.maxint)", default=sys.maxint)
+
+
+    (options, args)=parser.parse_args()
+    if options.infotag == "": 
+        sys.stderr.write("provide a value for --info parameter!\n")
+        exit(1)
+
+    vcfilename=args[0]
+    vcfh=open(vcfilename,'r')
+
+    #instantiate a VcfFile object
+    vcfobj=VcfFile(vcfilename)
+    #parse its metainfo lines (ones that begin with ##)
+    vcfobj.parseMetaLines(vcfh)
+    descriptors = vcfobj.getMetaInfoDescription()
+    infoids=[]
+    for (tag, description) in descriptors:
+        infoids.append(tag)
+
+    if options.infotag  not in infoids and options.infotag != 'QUAL':
+        sys.stderr.write(options.infotag + " tag not in ##INFO headers!\n")
+        exit(1)
+
+    vcfh.seek(0)
+    vcfobj.parseHeaderLine(vcfh)
+
+    pattern=options.infotag+'=(\d+?\.?\d+)'
+
+    infovalues=[]
+    for vrec in vcfobj.yieldVcfRecord(vcfh):
+        
+
+        if options.infotag == 'QUAL':
+            if float(vrec.getQual() ) >= float(options.max):
+                continue
+            else:
+                infovalues.append(float(vrec.getQual() ) )
+        else:
+            searchresult=re.search(pattern, vrec.getInfo() )
+            if re.search(pattern, vrec.getInfo() ) == None:
+                continue
+            else:
+                value=re.search(pattern, vrec.getInfo() ).groups()[0]
+                if float(value) >= float(options.max): continue
+                infovalues.append(float(value))
+
+    
+
+    
+
+
+    grdevices = importr('grDevices')
+    plotname=options.infotag+".png"
+    grdevices.png(file=plotname,width=512, height=512)
+
+    r = robjects.r
+   
+
+    x = robjects.FloatVector(infovalues)
+    ecdf = robjects.r['ecdf']
+    summary=robjects.r['summary']
+    print summary(x)
+    Fn = ecdf(x)
+    percentiles=Fn(x)
+    filename=vcfilename + "\n percentile plot " + options.infotag 
+    sys.stderr.write("making R plot ...\n")
+    r.plot(percentiles,x, xlab="percentile", ylab=options.infotag, main=filename)
+    grdevices.dev_off()
+
+    sys.stderr.write("see R plot in "+ plotname+"\n")
+
+if __name__ == "__main__":
+    main()

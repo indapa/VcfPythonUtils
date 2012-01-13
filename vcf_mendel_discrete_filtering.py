2e1346b98aa65254074ee8aa8ea6aa02269af368
indapa <indapa@gmail.com>
2012-01-13 18:41:27 -0500

    added lambda filter function

--- vcf_mendel_discrete_filtering.py
+++ vcf_mendel_discrete_filtering.py
@@ -9,6 +9,7 @@ from VcfFile import *
 
 """ look for shared variants in a list of affecteds given an inheritance model """
 
+
 def main():
     usage = "usage: %prog [options] file.vcf"
     parser = OptionParser(usage)
@@ -30,14 +31,27 @@ def main():
 
     #instantiate a VcfFile object
     vcfobj=VcfFile(vcfilename)
+    vcfobj.parseHeaderLine(vcfh)
     samplelist=vcfobj.getSampleList()
+    print "samplelist", samplelist
+    vcfh.seek(0)
     #parse its metainfo lines (ones that begin with ##)
     vcfobj.parseMetaLines(vcfh)
 
     for vrec in vcfobj.yieldVcfRecordwithGenotypes(vcfh ):
+        
+        affected_genotypes=[] #list of tuples (sample, genotypeobj) with samples that are affected
         if vrec.getFilter() != options.filter and options.filter != None : continue
+        genotypes = vrec.getGenotypesAlleles()
         genotype_tuple= vrec.zipGenotypes(samplelist)
-        print genotype_tuple
-
+        for (sample, genotype) in genotype_tuple:
+            if sample in affecteds:
+                affected_genotypes.append( ( sample, genotype.isSegregating() )  )
+        #print affected_genotypes
+        shared_affected_segregating = filter( lambda x, segregating=True: segregating in x, affected_genotypes)
+        #now if all affects are segregating for the site, its a candidate
+        #print len(shared_affected_segregating), len(affecteds), shared_affected_segregating, affected_genotypes
+        if len(shared_affected_segregating) == len(affecteds):
+            print shared_affected_segregating
 if __name__ == "__main__":
     main()

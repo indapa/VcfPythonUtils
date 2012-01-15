86bf7a741ebf1dd14cd7f006ffe4743af875f1d1
indapa <indapa@gmail.com>
2012-01-15 12:05:08 -0500

    print datalines of records shared amongst all affecteds

--- vcf_mendel_discrete_filtering.py
+++ vcf_mendel_discrete_filtering.py
@@ -31,27 +31,32 @@ def main():
 
     #instantiate a VcfFile object
     vcfobj=VcfFile(vcfilename)
+    #print the ## header lines
+    vcfobj.parseMetaLines(vcfh)
+    vcfobj.printMetaLines()
+    vcfh.seek(0)
+
+    #part the #Chrom header line
     vcfobj.parseHeaderLine(vcfh)
+    vcfobj.printHeaderLine()
     samplelist=vcfobj.getSampleList()
     print "samplelist", samplelist
-    vcfh.seek(0)
-    #parse its metainfo lines (ones that begin with ##)
-    vcfobj.parseMetaLines(vcfh)
-
+    
     for vrec in vcfobj.yieldVcfRecordwithGenotypes(vcfh ):
         
         affected_genotypes=[] #list of tuples (sample, genotypeobj) with samples that are affected
         if vrec.getFilter() != options.filter and options.filter != None : continue
         genotypes = vrec.getGenotypesAlleles()
-        genotype_tuple= vrec.zipGenotypes(samplelist)
-        for (sample, genotype) in genotype_tuple:
-            if sample in affecteds:
-                affected_genotypes.append( ( sample, genotype.isSegregating() )  )
-        #print affected_genotypes
+        genotype_tuple= vrec.zipGenotypes(samplelist) # get a list of tuples [ (sample, VcfGenotype object) ... ]
+        for (sample, genotype) in genotype_tuple: #iterate thru and see if they are in affected list
+            if sample in affecteds:  # if so ...
+                affected_genotypes.append( ( sample, genotype.isSegregating() )  ) # are they segregating for a non-ref allele?
+        #filter the collected samples to see if they are all have segregating genotypes
         shared_affected_segregating = filter( lambda x, segregating=True: segregating in x, affected_genotypes)
+
         #now if all affects are segregating for the site, its a candidate
-        #print len(shared_affected_segregating), len(affecteds), shared_affected_segregating, affected_genotypes
         if len(shared_affected_segregating) == len(affecteds):
             print shared_affected_segregating
+            print vrec.toStringwithGenotypes()
 if __name__ == "__main__":
     main()

a609af197a701fe7632d54f994971851f2f5b66f
indapa <indapa@gmail.com>
2012-05-01 17:02:22 -0400

    updated code to parse metalines

--- vcf_concat.py
+++ vcf_concat.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 import sys
 import os
 import string
@@ -8,33 +8,34 @@ from optparse import OptionParser
 from VcfFile import *
 
 """ concatenate an arbritary number of VCF files into a single file and print to STDOUT
-    note - doesn't check for equivalent metadata information across files             """
+    NOTE: - doesn't check for equivalent metadata ## lines  across files             """
 
 def main():
     usage = "usage: %prog  --in file.vcf --in file2.vcf [ ... ] "
     parser = OptionParser(usage)
     parser.add_option("--in", action="append", dest="vcf_list", help="file to concatenate")
-
+    parser.add_option("--filelist", type="string", dest="filelist", help="file with list of vcf files to concat", default=None)
+    parser.add_option("--printmetalines", action="store_true", dest="metalines", default=False, help="print metalines beginning with ##")
     (options, args)=parser.parse_args()
     commandline=" ".join(sys.argv)
     print "##commandLine " + commandline
-    printmetalines=False
+   
+
     
     for file in options.vcf_list:
         vcfh=open(file, 'r')
         vcfobj=VcfFile(file)
         #parse its metainfo lines (ones that begin with ##)
-        if printmetalines == False:
-            vcfobj.parseMetaLines(vcfh)
-            vcfobj.printMetaLines()
-            vcfh.seek(0)
-            vcfobj.parseHeaderLine(vcfh)
-            vcfobj.printHeaderLine()
-            printmetalines=True
-            vcfh.seek(0)
-            
-        for line in vcfobj.yieldVcfDataLine(vcfh):
-            print line
+        vcfobj.parseMetaAndHeaderLines(vcfh)
+        
+        if options.metalines == False:
+            for line in vcfobj.yieldVcfDataLine(vcfh):
+                print line
+            else:
+                vcfobj.printMetaInfoLines()
+                for line in vcfobj.yieldVcfDataLine(vcfh):
+                    print line
+       
 
 if __name__ == "__main__":
     main()

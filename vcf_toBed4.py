ac275637463e42086a85c963fd591a34bd62d344
indapa <indapa@gmail.com>
2012-05-17 14:55:13 -0400

    added dump option to dump out rest of vcf record in the id column of the bed

--- vcf_toBed4.py
+++ vcf_toBed4.py
@@ -12,6 +12,7 @@ def main():
     parser = OptionParser(usage)
     parser.add_option("--filter", type="string", dest="filter", help="extract records matching filter (default is None)", default=None)
     parser.add_option("--addchr", action="store_true", dest="addchr",  help="pre-pend 'chr' to chrom column ", default=False)
+    parser.add_option("--dump", action="store_true", dest="dump", help="dump everything after teh ID column in the 4th bed column")
     (options, args)=parser.parse_args()
 
     vcfilename=args[0]
@@ -27,11 +28,18 @@ def main():
     for dataline in vcfobj.yieldVcfDataLine(vcfh):
         fields=dataline.strip().split('\t')
         (chrom,pos,id,ref,alt,qual,filtercode,info)=fields[0:8]
+        
         if options.addchr ==True:
             chrom='chr'+chrom
         if filtercode != options.filter and options.filter != None : continue
         (start,end) = (int(pos)-1, int(pos))
-        bedstring= "\t".join( [ chrom, str(start), str(end), id ] )
+        if options.dump == True:
+            gstrings=",".join(fields[8::])
+            dumpstring=";".join([ref,alt,qual,filtercode,info,gstrings])
+            bedstring= "\t".join( [ chrom, str(start), str(end), id ,dumpstring] )
+        else:
+            bedstring= "\t".join( [ chrom, str(start), str(end), id] )
+
         print bedstring
 if __name__ == "__main__":
     main()

6535a41c1ef71d4745948eb369fbcf9f264ca474
indapa <indapa@gmail.com>
2012-05-21 09:56:12 -0400

    print comparison matrix after comparison

--- vcf_genotype_concordance.py
+++ vcf_genotype_concordance.py
@@ -368,6 +368,9 @@ def main():
         #print nrc
         #print nrd
         gtm= discordance_dict[sample]
+        print sample
+        print gtm
+        print "\n"
         eval_nocalls = gtm[3,0] + gtm[3,1] + gtm[3,2] + gtm[3,3]
         comparison_nocalls = gtm[0,3] + gtm[1,3] + gtm[2,3] + gtm[3,3]
         outstring = "\t".join( [ sample, str(nrd), str(nrc),  str ( np.sum( discordance_dict[sample] ) ) , str(eval_nocalls) , str(comparison_nocalls ) ] )

867ac5dbe1e2412da032ff8e9ed3718891d042aa
indapa <indapa@gmail.com>
2012-05-23 16:57:38 -0400

    added conditional checks for biallelic genotype

--- vcf_genotypeSummary.py
+++ vcf_genotypeSummary.py
@@ -12,16 +12,20 @@ def typeofGenotype(allele1, allele2):
     """ I really should be a python version of a typedef here, but dont know how
         hom_ref =0 het =1 hom_nonref=2 no_call=3 """
 
-    if allele1 == '0' and allele2 == '0': return 0
-
-    if allele1 == '0' and allele2== '1': return 1
-    if allele1 =='1' and allele2 == '0': return 1
-
-    if allele1== '1' and allele2== '1': return 2
-
-    if allele1== '.' or allele2 == '.': return 3
-    if allele1 == '.' and allele2 == '.': return 3
-
+    if allele1 == '0' and allele2 == '0':
+        return 0
+    elif allele1 == '0' and allele2== '1':
+        return 1
+    elif allele1 =='1' and allele2 == '0':
+        return 1
+    elif allele1== '1' and allele2== '1':
+        return 2
+    elif allele1== '.' or allele2 == '.':
+        return 3
+    elif allele1 == '.' and allele2 == '.':
+        return 3
+    else:
+        return None
 
 
 def get_genotype_counts(g):
@@ -66,13 +70,12 @@ def main():
         vrec_ziptuple=vrec.zipGenotypes(samples)
         genotype_typecounts=get_genotype_counts(vrec_ziptuple)
         for (g, sample) in genotype_typecounts:
-            #print g, sample
+            if g == None: continue
             genotype_dict[sample][g]+=1
-
+    #sample homoz_ref het homoz_nonref nocall
     for sample in genotype_dict.keys():
         print sample
-        print genotype_dict[sample]
-        print 
+        outstring = "\t".join( map(str,genotype_dict[sample]) )
 
 
 if __name__ == "__main__":

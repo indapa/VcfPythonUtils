e49370ca4d7f8438f297098325717761371dcd37
indapa <indapa@gmail.com>
2012-06-15 19:13:15 -0400

    corrected incorrect indentation in for loop

--- variantEvalGenotypeConcordance.py
+++ variantEvalGenotypeConcordance.py
@@ -49,26 +49,23 @@ def main():
             continue
         vrec_ziptuple=vrec.zipGenotypes(samples)
         for (compare, eval) in grouper(2,vrec_ziptuple):
-        #print compare, eval
             (comp_allele1, comp_allele2)=compare[1].getAlleles()
             (eval_allele1, eval_allele2)=eval[1].getAlleles()
             eval_alleletype=typeofGenotype(eval_allele1, eval_allele2)
             comp_alleletype=typeofGenotype(comp_allele1, comp_allele2)
-        #print compare[0], comp_alleletype, eval[0], eval_alleletype, vrec.getChrom(), vrec.getPos()
-        #print compare[1].toString(), eval[1].toString()
             concordancetable[eval_alleletype, comp_alleletype]+=1
-            print concordancetable
+       
 
-            discordance=concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,0]+concordancetable[1,2]+concordancetable[2,0]+concordancetable[2,1]
-            total=concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,0]+concordancetable[1,1]+ concordancetable[1,2]+concordancetable[2,0]+concordancetable[2,1] +concordancetable[2,2]
-#print total, discordance
-            nrd=round( (float(discordance)/float(total)) * 100, 2)
-            variant_count_evaluation= concordancetable[1,1]+ concordancetable[1,2]+ concordancetable[2,1]+ concordancetable[2,2]
-            variant_count_comparison= concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,1]+concordancetable[1,2]+concordancetable[2,1]+concordancetable[2,2]+concordancetable[3,1]+concordancetable[3,2]
-            nrs=round( float(variant_count_evaluation)/float(variant_count_comparison) * 100 , 2)
+    discordance=concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,0]+concordancetable[1,2]+concordancetable[2,0]+concordancetable[2,1]
+    total=concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,0]+concordancetable[1,1]+ concordancetable[1,2]+concordancetable[2,0]+concordancetable[2,1] +concordancetable[2,2]
 
-            print "NRD: ", str(nrd)
-            print "NRS ", str(nrs)
+    nrd=round( (float(discordance)/float(total)) * 100, 2)
+    variant_count_evaluation= concordancetable[1,1]+ concordancetable[1,2]+ concordancetable[2,1]+ concordancetable[2,2]
+    variant_count_comparison= concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,1]+concordancetable[1,2]+concordancetable[2,1]+concordancetable[2,2]+concordancetable[3,1]+concordancetable[3,2]
+    nrs=round( float(variant_count_evaluation)/float(variant_count_comparison) * 100 , 2)
+    
+    print "NRD: ", str(nrd)
+    print "NRS ", str(nrs)
 # <codecell>
 
 if __name__ == "__main__":

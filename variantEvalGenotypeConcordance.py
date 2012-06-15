a33b1ba043f18c91916aecf7d1365a8df272051c
indapa <indapa@gmail.com>
2012-06-15 19:09:47 -0400

    proper command line parsing; defined main method

--- variantEvalGenotypeConcordance.py
+++ variantEvalGenotypeConcordance.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
 from itertools import *
+from VcfFile import *
+import numpy as np
+
+from optparse import OptionParser
 
 def typeofGenotype(allele1, allele2):
     """ I really should be a python version of a typedef here, but dont know how
@@ -22,46 +27,50 @@ def grouper(n, iterable, fillvalue=None):
     return izip_longest(fillvalue=fillvalue, *args)
 
 
-from VcfFile import *
-import numpy as np
-
+def main():
+    usage = "usage: %prog [options] file.vcf"
+    parser = OptionParser(usage)
+    (options, args)=parser.parse_args()
+    vcfilename=args[0]
 #row is veal column is comparison
-concordancetable= np.matrix( [ [ 0,0,0,0 ], [ 0,0,0,0 ], [ 0,0,0,0 ], [ 0,0,0,0 ] ] )
+    concordancetable= np.matrix( [ [ 0,0,0,0 ], [ 0,0,0,0 ], [ 0,0,0,0 ], [ 0,0,0,0 ] ] )
 
 
 
-vcfobj=VcfFile('gatkuf.20120527.sample3_4.wga.genomic.merge.vcf')
-vcfh=open('gatkuf.20120527.sample3_4.wga.genomic.merge.vcf','r')
+    vcfobj=VcfFile(vcfilename)
+    vcfh=open(vcfilename,'r')
 
-vcfobj.parseMetaAndHeaderLines(vcfh)
-samples=vcfobj.getSampleList()
-for vrec in vcfobj.yieldVcfRecordwithGenotypes(vcfh):
-    if len(vrec.getAlt()) > 1: continue
-    if 'filterIn' in vrec.getInfo(): 
+    vcfobj.parseMetaAndHeaderLines(vcfh)
+    samples=vcfobj.getSampleList()
+    for vrec in vcfobj.yieldVcfRecordwithGenotypes(vcfh):
+        if len(vrec.getAlt()) > 1: continue
+        if 'filterIn' in vrec.getInfo(): 
         #print vrec.toString()
-        continue
-    vrec_ziptuple=vrec.zipGenotypes(samples)
-    for (compare, eval) in grouper(2,vrec_ziptuple):
+            continue
+        vrec_ziptuple=vrec.zipGenotypes(samples)
+        for (compare, eval) in grouper(2,vrec_ziptuple):
         #print compare, eval
-        (comp_allele1, comp_allele2)=compare[1].getAlleles()
-        (eval_allele1, eval_allele2)=eval[1].getAlleles()
-        eval_alleletype=typeofGenotype(eval_allele1, eval_allele2)
-        comp_alleletype=typeofGenotype(comp_allele1, comp_allele2)
+            (comp_allele1, comp_allele2)=compare[1].getAlleles()
+            (eval_allele1, eval_allele2)=eval[1].getAlleles()
+            eval_alleletype=typeofGenotype(eval_allele1, eval_allele2)
+            comp_alleletype=typeofGenotype(comp_allele1, comp_allele2)
         #print compare[0], comp_alleletype, eval[0], eval_alleletype, vrec.getChrom(), vrec.getPos()
         #print compare[1].toString(), eval[1].toString()
-        concordancetable[eval_alleletype, comp_alleletype]+=1
-print concordancetable
+            concordancetable[eval_alleletype, comp_alleletype]+=1
+            print concordancetable
 
-discordance=concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,0]+concordancetable[1,2]+concordancetable[2,0]+concordancetable[2,1]
-total=concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,0]+concordancetable[1,1]+ concordancetable[1,2]+concordancetable[2,0]+concordancetable[2,1] +concordancetable[2,2]
+            discordance=concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,0]+concordancetable[1,2]+concordancetable[2,0]+concordancetable[2,1]
+            total=concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,0]+concordancetable[1,1]+ concordancetable[1,2]+concordancetable[2,0]+concordancetable[2,1] +concordancetable[2,2]
 #print total, discordance
-nrd=round( (float(discordance)/float(total)) * 100, 2)
-variant_count_evaluation= concordancetable[1,1]+ concordancetable[1,2]+ concordancetable[2,1]+ concordancetable[2,2]
-variant_count_comparison= concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,1]+concordancetable[1,2]+concordancetable[2,1]+concordancetable[2,2]+concordancetable[3,1]+concordancetable[3,2]
-nrs=round( float(variant_count_evaluation)/float(variant_count_comparison) * 100 , 2)
+            nrd=round( (float(discordance)/float(total)) * 100, 2)
+            variant_count_evaluation= concordancetable[1,1]+ concordancetable[1,2]+ concordancetable[2,1]+ concordancetable[2,2]
+            variant_count_comparison= concordancetable[0,1]+concordancetable[0,2]+concordancetable[1,1]+concordancetable[1,2]+concordancetable[2,1]+concordancetable[2,2]+concordancetable[3,1]+concordancetable[3,2]
+            nrs=round( float(variant_count_evaluation)/float(variant_count_comparison) * 100 , 2)
 
-print "NRD: ", str(nrd)
-print "NRS ", str(nrs)
+            print "NRD: ", str(nrd)
+            print "NRS ", str(nrs)
 # <codecell>
 
+if __name__ == "__main__":
+    main()
 

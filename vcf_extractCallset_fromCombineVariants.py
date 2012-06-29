b560c772fc6b72d086474b415e976f6cbf9969f0
indapa <indapa@gmail.com>
2012-06-29 15:55:03 -0400

    added --v complemnet option which prints records not matching set

--- vcf_extractCallset_fromCombineVariants.py
+++ vcf_extractCallset_fromCombineVariants.py
@@ -12,13 +12,14 @@ def main():
     usage = "usage: %prog [options] merged.vcf"
     parser = OptionParser(usage)
     parser.add_option("--extract", type="string", dest="extract", help="set to extract", default=None)
-
+    parser.add_option("--v", type="string", dest="complement", help="extract the complement of this set", default=None)
     (options, args)=parser.parse_args()
     vcfile=args[0]
     if options.extract != None:
         pattern='set=('+options.extract+')'
+    
     else:
-        pattern='set=(\w+)'
+        pattern='set=(.+)'
     
     callsetCounter=Counter()
 
@@ -26,21 +27,26 @@ def main():
     vcfh=open(vcfile,'r')
 
     vcfobj.parseMetaAndHeaderLines(vcfh)
-    if options.extract != None:
+    if options.extract != None or options.complement != None:
         vcfobj.printMetaAndHeaderLines()
-    samples=vcfobj.getSampleList()
-    for vrec in vcfobj.yieldVcfRecordwithGenotypes(vcfh):
     
+    for vrec in vcfobj.yieldVcfRecordwithGenotypes(vcfh):
+
         if re.search(pattern, vrec.getInfo() ) == None:
             continue
         else:
             value=re.search(pattern, vrec.getInfo() ).groups()[0]
-        #print value
+        
             callsetCounter[value]+=1
-        if options.extract != None:
-            print vrec.toStringwithGenotypes()
+            if options.complement != None:
+                if value != options.complement:
+                    print vrec.toStringwithGenotypes()
 
-    if options.extract == None:
+            if options.extract != None:
+                print vrec.toStringwithGenotypes()
+           
+
+    if options.extract == None and options.complement == None:
         for (type,count) in callsetCounter.items():
             print type, str(count)
         print "total: " + str( sum(callsetCounter.values()) )

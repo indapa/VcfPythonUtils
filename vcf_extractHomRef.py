f7e0359dc46897b3a4f4eed54182666b3d289e99
indapa <indapa@gmail.com>
2012-08-08 11:54:30 -0400

    initial commit

---
+++ vcf_extractHomRef.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import os
+import string
+import re
+from optparse import OptionParser
+from VcfFile import *
+
+
+def main():
+    """ extract homozygous reference records from a VCF file  """
+    usage = "usage: %prog [options] file.vcf"
+    parser = OptionParser(usage)
+    #parser.add_option("--name", type="string|int|boolean", dest="name", help="help string")
+
+    (options, args)=parser.parse_args()
+    vcfilename=args[0]
+    vcfobj=VcfFile(vcfilename)
+    vcfh=open(vcfilename,'r')
+    vcfobj.parseMetaAndHeaderLines(vcfh)
+
+    for vrec in vcfobj.yieldVcfRecordwithGenotypes(vcfh):
+        if vrec.getAlt() == ".":
+            print vrec.toStringWithGenotypes()
+        
+
+
+if __name__ == "__main__":
+    main()

743f7fb364e10a590c140845f57284e4b2c16362
Amit Indap <indapa@gmail.com>
2013-04-20 17:25:29 -0400

    initial commit vcf_nrdLog_analysis.py

--- vcf_nrdLog_analysis.py
+++ vcf_nrdLog_analysis.py
@@ -4,8 +4,8 @@ from VcfFile import *
 import numpy as np
 import re, os
 from optparse import OptionParser
-from common import grouper
-from common import typeofGenotype
+from common import grouper, typeofGenotype, melt_lol
+
 
 def main():
     usage = "usage: %prog [options]  nrd.log.vcf\n" 
@@ -22,15 +22,30 @@ def main():
 
     vcfobj.parseMetaAndHeaderLines(vcfh)
     samples=vcfobj.getSampleList()
-    print samples
-    
+    #print samples
+    #print "#setname\t" + "\t".join(samples) 
     for vrec in vcfobj.yieldVcfRecordwithGenotypes(vcfh):
+        outputline=[[ vrec.getPos()] ]
+        
+        setname=vrec.returnInfoDict()['set'] #which callset does the site belong to?
+        
+        outputline.append(  [setname] ) #we aggregate genotypes per sample heere
+        
         vrec_ziptuple=vrec.zipGenotypes(samples)
+        #print vrec_ziptuple
         for (compare, eval) in grouper(2,vrec_ziptuple):
-            print compare
-            print eval
-            print
-        print
+            (comp_allele1, comp_allele2)=compare[1].getAlleles()
+            (eval_allele1, eval_allele2)=eval[1].getAlleles()
+            eval_alleletype=typeofGenotype(eval_allele1, eval_allele2)
+            comp_alleletype=typeofGenotype(comp_allele1, comp_allele2)
+            if eval_alleletype == comp_alleletype:
+                continue
+            outputline.append(   [ eval[0], str(eval_alleletype), compare[0], str(comp_alleletype) ] )
+        
+        
+        print "\t".join (  melt_lol(outputline) )
+            
+        
         
 
 if __name__ == "__main__":
